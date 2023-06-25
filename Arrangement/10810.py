bb8b39eaae87e950b1fc2c2f63136343d4eae922
0E <92708940+Yum02@users.noreply.github.com>
2023-06-25 14:44:02 +0000

    create

--- Arrangement/10810.py
+++ Arrangement/10810.py
@@ -1,2 +1,6 @@
 N, M = map(int, input().split())
-
+basket = [0]*N
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    basket[(i-1):j] = [k] * (j-i+1)
+print(' '.join(map(str, basket)))
