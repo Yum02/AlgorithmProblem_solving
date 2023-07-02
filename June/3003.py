659df1a7139e194c17793f9dd8b9155ab9ba7909
0E <92708940+Yum02@users.noreply.github.com>
2023-07-02 09:17:28 +0000

    0

---
+++ June/3003.py
@@ -0,0 +1,9 @@
+cs_list = [1, 1, 2, 2, 2, 8]
+a, b, c, d, e, f = map(int, input().split())
+
+print(cs_list[0] - a, end=" ")
+print(cs_list[1] - b, end=" ")
+print(cs_list[2] - c, end=" ")
+print(cs_list[3] - d, end=" ")
+print(cs_list[4] - e, end=" ")
+print(cs_list[5] - f, end=" ")
